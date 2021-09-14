238a529f1e6a092adb2632786812f5445c229ac5
Code Hopper <90166401+serpentuin@users.noreply.github.com>
2021-09-14 11:12:43 +0800

    Add files via upload

---
+++ copy_model_config_to_custom_model.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+from create_folders import files, paths, scripts
+
+src = os.path.join(paths['PRETRAINED_MODEL_PATH'], files['PRETRAINED_MODEL_NAME'], scripts['pipeline.config'])
+dst = os.path.join(paths['CHECKPOINT_PATH'])
+shutil.copyfile(src=src, dst=dst)
