0b1c712e5595669c9b43c7460719188da743466e
Aqmals <90166401+serpentuin@users.noreply.github.com>
2021-10-25 22:54:25 +0800

    Update copy_model_config_to_custom_model.py

--- copy_model_config_to_custom_model.py
+++ copy_model_config_to_custom_model.py
@@ -2,6 +2,6 @@ import os
 import shutil
 from create_folders import files, paths, scripts
 
-src = os.path.join(paths['PRETRAINED_MODEL_PATH'], files['PRETRAINED_MODEL_NAME'], scripts['PIPELINE_CONFIG'])
+src = os.path.join(paths['PRETRAINED_MODEL_PATH'], files['PRETRAINED_MODEL_NAME'], 'pipeline.config')
 dst = os.path.join(paths['CHECKPOINT_PATH'])
 shutil.copyfile(src=src, dst=dst)
