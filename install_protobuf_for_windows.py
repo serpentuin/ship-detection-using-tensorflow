6387df6560c517b4241990e0e5aca5d377c38134
Aqmals <90166401+serpentuin@users.noreply.github.com>
2022-03-01 08:23:10 +0800

    Update install_protobuf_for_windows.py

--- install_protobuf_for_windows.py
+++ install_protobuf_for_windows.py
@@ -7,9 +7,9 @@ from create_folders import files, paths, scripts
 
 # Install Tensorflow Object Detection 
 if os.name=='nt':
-    url="https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/protoc-3.17.3-win64.zip"
+    url="https://github.com/protocolbuffers/protobuf/releases/download/v3.19.4/protoc-3.19.4-win64.zip"
     wget.download(url)
-    shutil.move('protoc-3.17.3-win64.zip', paths['PROTOC_PATH'])
+    shutil.move('protoc-3.19.4-win64.zip', paths['PROTOC_PATH'])
     os.chdir(paths['PROTOC_PATH'])
-    with ZipFile('protoc-3.17.3-win64.zip', 'r') as zipObj:
+    with ZipFile('protoc-3.19.4-win64.zip', 'r') as zipObj:
         zipObj.extractall()
